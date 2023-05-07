6d9f581ab98945064239c548bb8542d0aed29bda
Pob <jeduardoa2003@gmail.com>
2023-05-07 00:06:42 -0400

    .

--- index.js
+++ index.js
@@ -345,7 +345,7 @@ function checkWinner(win){
     win ? console.log("You Win") : console.log("You lose");
 } */
 
-// &[template literals | HTML         -   01:53:25]
+// &[template literals | HTML         -   01:53:25] "alt+96"
 /* 
 // delimitadas con " ` ", permite tener "embedded" variables y expresiones, se pone el string tipo
 // (` Hello ${variable} `);
@@ -618,15 +618,323 @@ function print(element){console.log(element);}
  */
 
 // &[array.filter() |          -   02:48:10]
+/*
+//+ array.filter() = creates a new array with all the elements that pass the test
+//+                  provided by a function passed
+
+let ages = [18, 16, 21, 17, 19, 90];
+let adults = ages.filter(checkAge);
+//+ Basicamente crea un array totalmente nuevo, con los elementls del original que cumplan la condicion
+//+ condicion que nosotros definimos en la funcion que le pasamos
+
+adults.forEach(print);
+
+function print(element){
+	console.log(element);
+}
+
+function checkAge(element){
+	return element >= 18;
+}
+*/
+
 // &[array.reduce() |          -   02:50:21]
+/*
+//+ array.reduce() = Reduce el array a 1 solo valor
+//+ Basicamente junta todos los valores del array en 1 solo, en caso de numeros, los suma todos esos valores numericos de 1
+
+let prices = [5, 10, 15, 20, 25, 30];
+let total = prices.reduce(checkOut);
+
+console.log("The total is", total, "$");
+
+function checkOut(total, element){
+				return total - element;
+}
+*/
+
 // &[sort an array of numbers |          -   02:52:33]
-// &[function expressions |          -   02:54:41]
+/*
+let grades = [100, 50, 90, 60, 80, 70];
+
+grades = grades.sort(descendingSort);
+grades.forEach(print); //+ Cuidado con el forEach aqui, porque en los logs, toma cada "instancia" del uso de la funcion como la misma linea, usar console.log() dentro de la propia funcion para debug
+
+console.log("===[Divider]===");
+grades = grades.sort(ascendingSort);
+grades.forEach(print);
+
+
+function print(element){
+				console.log(element);
+}
+function descendingSort(x, y){
+				return y-x;
+}
+function ascendingSort(x, y){
+				return x-y;
+}
+*/
+
+// &[function expressions | HTML         -   02:54:41]
+/*
+//+ function expression = Una funcion sin un nombre ("Anonima"), evita "acaparar" el scope global con nombres.
+//+        "Escribir y Olvidarla"  
+
+const greeting = function() //+ Se puede hacer, pero es innecesario, lo dejo para demostrar lo de que la funcion la igualamos a un nombre, y la llamamos por dicho nombre
+{console.log("Hello!");}
+greeting();
+
+let count = 0;
+
+document.getElementById("increaseButton").onclick = function(){
+				count++;
+				document.getElementById("myLabel").innerHTML = count;
+}
+document.getElementById("decreaseButton").onclick = function(){
+				count--;
+				document.getElementById("myLabel").innerHTML = count;
+}
+*/
+
 // &[arrow function expressions |          -   02:59:55]
+/* 
+//+ arrow function = compact alternative to a traditional function expression
+//+					=>
+
+//+ Basicamente en vez de poner " function(param){} " ponemos " (param)=>{}"
+    const greeting = (userName)=>console.log("Hello", userName, "!");
+//+ puede usarse con o sin { } si la funcion tiene 1 sola declaracion, igual con los params, si es solo 1 se puede usar sin los ()
+greeting("Bro");
+
+const percent = (x,y)=> x/y*100;    //+ Si es sin los {} no se debe poner el return o da error
+console.log(`${percent(45, 50)}%`);
+
+//+ mismo codigo del ejemplo del sort array of numbers, pero con arrow espression al ser funciones de 1 sola declaracion
+let grades = [100, 50, 90, 60, 80, 70];
+
+grades.sort((x, y) => {return y-x;});
+grades.forEach((element) => console.log(element));
+ */
+
 // &[shuffle an array |          -   03:05:42]
+/* 
+//+ Basicamente usar el random y cambiar posiciones de un array
+let cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+shuffle(cards);
+console.log(cards);
+cards.forEach((card) => console.log(card));
+
+function shuffle(array){
+    let currentIndex = array.length;
+    while(currentIndex != 0){   //Empieza por el final
+        let randomIndex = Math.floor(Math.random() * array.length);
+        currentIndex-=1;
+        
+        let temporary = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporary;
+        //Por cada posicion, se busca un umero random menor, por el que cambiar posicion, y seguir 
+        // el mismo proceso en donde se iba
+    }
+    return array;
+} */
+
 // &[nested functions |          -   03:09:30]
+/* 
+//+ nested functions = Funcion dentro de otra funcion.
+//+                     Las funciones de "afuera" tienen acceso a las de adentro
+//+                     Las de adentro estan "escondidas" desde afuera
+//+                     usado en encapsulamiento
+
+let userName = "Bro";
+let userInbox = 10;
+login();
+
+//+ Ocultamos las funciones de "adentro" definiendolas directamente dentro de la otra funcion
+//+ Truqueando el scope de esa forma
+function login(){
+    displayUserName();
+    displayUserInbox();
+
+    function displayUserName(){
+        console.log(`Welcome ${userName}`);
+    }
+    function displayUserInbox(){
+        console.log(`You have ${userInbox} new messages`);
+    }
+}
+ */
+
 // &[maps |          -   03:12:47]
+/* 
+//+ Map = Objeto que guarda valores en forma de "llave-valor" (como "pares", osea juntos) de cualquier tipo de data
+
+const store = new Map([
+    //+ [Llave, Valor] , [Llave, Valor] , ...
+    ["t-shirt", 20],
+    ["jeans", 30],
+    ["socks", 10],
+    ["underwear", 50]
+]);
+
+//+ Digamos que deseamos acceder a 1 de esos valores
+let shoppingCart = 0;
+shoppingCart += store.get("t-shirt");   //+ Accedemos a traves de la llave, como si la llave fuera el "index" en un array
+shoppingCart += store.get("underwear");
+console.log("ShoppingCart = ",shoppingCart);
+
+store.set("hat", 40);   //+ Para añadir valores, llave y valor
+store.delete("socks");  //+ Para eliminar, pasamos la llave
+
+console.log(`Does the Store has Hats?: ${store.has("hat")}`);   //+ Tiene una llave?
+console.log(`Does the Store has Socks?: ${store.has("socks")}`);
+
+console.log(store.size);    //+ Mirar la cant de pares
+
+//+ Para mostrar los valores
+store.forEach((value, key) => {console.log(`${key} = $${value}`);});
+ */
+
 // &[objects  |          -   03:17:07]
+/* 
+//+ object = Un grupo de propiedades y metodos
+//+         Propiedades = lo que un objeto tiene
+//+         Metodos = lo que un objeto puede hacer
+//+             Se usa " . " para acceder a las propiedades y metodos
+
+const car1 = {
+    //& Propiedades
+    //+ Basicamente variable : valor, parecido a map, pero aqui el valor se lo estamos asignando de una, lo cual usualmente no pasa, si no se usan los constructores de las clases
+    //+ Variables separadas por commas
+    model: "Mustang",
+    color: "red",
+    year: 2023,
+
+    //& Metodos 
+    //+ Basicamente variables que son funciones, como en Js las podemos igualar de esa forma, tons funciona
+    //+ Como son variables, o casi, pues se separan tambien por commas
+    drive : function(){     //! el " this " se ve en la sig leccion
+        console.log(`You Drive the car: ${this.model}, year: ${this.year}`);
+    },
+    break : function(){
+        console.log(`You Step on the break of the car: ${this.model}, year: ${this.year}`);
+    }
+}
+//+ Ya Creamos el Objeto, OBJETO directamente no CLASE
+console.log(car1.model);
+console.log(car1.color);
+console.log(car1.year);
+car1.drive();
+car1.break();
+
+
+//+ no es clase aun, tons pa crear otro objeto es a pata
+const car2 = {
+    //& Propiedades
+    //+ Basicamente variable : valor, parecido a map, pero aqui el valor se lo estamos asignando de una, lo cual usualmente no pasa, si no se usan los constructores de las clases
+    //+ Variables separadas por commas
+    model: "Corvette",
+    color: "Blue",
+    year: 2024,
+
+    //& Metodos 
+    //+ Basicamente variables que son funciones, como en Js las podemos igualar de esa forma, tons funciona
+    //+ Como son variables, o casi, pues se separan tambien por commas
+    drive : function(){     //! el " this " se ve en la sig leccion
+        console.log(`You Drive the car: ${this.model}, year: ${this.year}`);
+    },
+    break : function(){
+        console.log(`You Step on the break of the car: ${this.model}, year: ${this.year}`);
+    }
+}
+console.log(car2.model);
+console.log(car2.color);
+console.log(car2.year);
+car2.drive();
+car2.break();
+ */
+
 // &[this keyword |          -   03:21:09]
+/* 
+//+ this = Referencia para a un objeto particular (misma instancia de dicho objeto)
+//+         Depende del contexto inmediato a su alrededor
+
+const car1 = {
+    //& Propiedades
+    model: "Mustang",
+    color: "Red",
+    year: 2023,
+
+    //& Metodos 
+    drive : function(){
+        //+ Tenemos que usar el " this. " para asi poder acceder a los datos/variables de ESTE objeto en especifico
+        //+ Si no, no lo podemos acceder
+        //+ Funciona igual si podemos el nombre del propio objeto (pero esto mayormente se desconce, al usar objetos como "anonimos" a traves de arrays 
+        //+ (accediendo a cual es cual a traves de posiciones en el array))
+        console.log(`You Drive the car: ${this.model}, year: ${car1.year}`);
+    }
+}
+const car2 = {
+    //& Propiedades
+    model: "Corvette",
+    color: "Blue",
+    year: 2024,
+
+    //& Metodos 
+    drive : function(){
+        console.log(`You Drive the car: ${this.model}, year: ${this.year}`);
+    }
+}
+
+console.log(car1.model);
+console.log(car1.color);
+console.log(car1.year);
+car1.drive();
+
+console.log(this);
+//+ Contexto INMEDIATO, como realmente en el HTML nos encontramos en una "window", nos retorna esta misma
+//+ lo cual si lo analizamos, nos permite cambiar propiedades de la propia window
+this.name = "MyCoolWindow";
+console.log(this , "this.name =", this.name);
+ */
+
 // &[classes  |          -   03:24:08]
+/* 
+//+ class = Un "Plano" para crear objetos
+//+         Define que propiedades y metodos tenen los Objetos creados a partir de la clase
+//+         usa un "constructor" para propiedades unicas
+
+//+ se crean usando "class [Nombre de la Clase, usualmente 1ra letra Mayuscula]"
+class Player{
+    //& Propiedades
+    //+ Son variables aunque no les pongamos el "let"
+    //+ Cuidado con Js, puesto que si con el " this. " intentamos acceder a una propiedad que "no existe" solo nos devolvera "undefined"
+    score = 0;
+
+    //& Metodos
+    //+ No es necesario usar el "function" para los Metodos de una clase
+    pause(){
+        console.log("You Paused the Game");
+    }   //+ No es necesario separar con commas, ya que esta delimitado ya por los {}
+    exit(){console.log("You Exited the Game");}
+}
+
+//+ Tenemos el Plano creado, pero eso, solo el plano, tenemos que crear el objeto a partir del plano ahora, o mejor dicho "Instanciar" la clase
+//+ se crea usando el "new [Class_name]()"
+const player1 = new Player();
+//+ Ventaja del uso de clases, es que nos permite crear objetos muy similares de forma muy rapida y sencilla, asi como reusar la clase las veces que deseemos
+const player2 = new Player();
+const player3 = new Player();
+const player4 = new Player();
+
+//+ al ya haberlo creado, podemos modificar sus propiedades/atributos y acceder a estos, es un objeto ya despues de todo
+player1.score += 500;
+console.log(player1.score);
+console.log(player2.score);
+player1.pause();
+player2.exit();
+ */
+
 // &[constructors |          -   03:27:15]
 // &[END |          -   ]
